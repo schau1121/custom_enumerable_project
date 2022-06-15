4c5fbd849d41e0c383df99714b2d6f11f1a16e2a
Simon Chau <simon.chau1121@gmail.com>
2022-06-15 15:58:13 -0700

    Passed all test cases

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,88 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return self unless block_given?
+
+    i = 0
+    for element in self do
+      yield element, i
+      i += 1
+    end
+  end
+
+  def my_all?
+    return self unless block_given?
+
+    for element in self do
+      return false if !yield(element)
+    end
+
+    return true
+  end
+
+  def my_any?
+    return self unless block_given?
+    
+    for element in self do
+      return true if yield(element)
+    end
+
+    return false
+  end
+
+  def my_none?
+    return self unless block_given?
+
+    for element in self do 
+      return false if yield(element)
+    end
+
+    return true
+  end
+
+  def my_count
+    return self.size unless block_given?
+
+    count = 0
+    for element in self do
+      count += 1 if yield(element)
+    end
+    
+    return count
+  end
+
+  def my_map
+    return self unless block_given?
+
+    out = Array.new
+    for element in self do
+      out << yield(element)
+    end
+
+    return out
+  end
+
+  def my_inject(initial_value)
+    return self unless block_given?
+
+    out = initial_value
+    for element in self do
+      out = yield(out, element)
+    end
+
+    return out
+  end
+
+  def my_select
+    return self unless block_given?
+
+    out = Array.new
+    for element in self do
+      out << element if yield(element)
+    end
+    
+    return out
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +91,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each 
+    return self unless block_given?
+    for element in self do
+      yield element
+    end
+  end
 end
